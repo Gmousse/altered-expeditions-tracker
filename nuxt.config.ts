aa1193ed22b1ae4650ffb222cbf6fbb5a7535b78
Guillaume Mousnier <mousnier.guillaume@gmail.com>
2024-10-27 11:03:27 +0100

    fix: disable ssr for client build only

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
-  css: ["normalize.css/normalize.css"],
-  typescript: { typeCheck: true },
-  modules: ["@nuxt/image", "@nuxt/icon", "@nuxt/ui"],
+    ssr: false,
+    compatibilityDate: "2024-04-03",
+    devtools: { enabled: true },
+    css: ["normalize.css/normalize.css"],
+    typescript: { typeCheck: true },
+    modules: ["@nuxt/image", "@nuxt/icon", "@nuxt/ui"],
+    hooks: {
+        "prerender:routes"({ routes }) {
+            routes.clear(); // Do not generate any routes (except the defaults)
+        },
+    },
 });
